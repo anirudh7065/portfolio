5637800e021db576dcd4a992d792eed7b74e53fa
anirudh7065 <107138932+anirudh7065@users.noreply.github.com>
2024-08-25 01:24:08 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const Footer = () => {
     return (
-        <footer className='bg-purple-900 text-black font-bold gap-3 justify-center flex items-center w-full h-8 fixed bottom-0'>
+        <footer className='bg-purple-900 text-black font-bold gap-3 justify-center flex items-center w-full h-8 fixed bottom-0 select-none'>
             <span>Made By Abhishek valsan</span>
             <div className="social flex">
                 <span>
